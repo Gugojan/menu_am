fef65bf0e3440cfad0000923952f16654863c5dd
Gugo <gugo.90@bk.ru>
2020-07-21 21:07:50 +0400

    event listener

---
+++ app/Listeners/OrderShippeListener.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\OrderShipped;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class OrderShippeListener
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  OrderShipped  $event
+     * @return void
+     */
+    public function handle(OrderShipped $event)
+    {
+//        dd($event);
+        return $event;
+    }
+}
